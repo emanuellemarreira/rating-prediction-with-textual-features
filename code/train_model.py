dfdd1b8b7a1982c170101cdc225be6df0866a468
emanuellemarreira <emanuellemarreira@gmail.com>
2025-05-18 16:36:40 -0400

    fix: added categoria_rating rq4

--- code/train_model.py
+++ code/train_model.py
@@ -59,6 +59,7 @@ class TrainModel:
             df_train.drop(df_train[df_train["categoria"] != self.category].index, inplace=True)
             df_test.drop(df_test[df_test["categoria"] != self.category].index, inplace=True)
             df = pd.concat([df_train, df_test])
+            df['categoria_rating'] = df['categoria'].astype(str) + "_" + df['rating'].astype(str)
             drop = ['categoria','text','rating','categoria_rating']
             X = df.drop(columns=drop)
             print(f"{self.category}: {X.shape}")
