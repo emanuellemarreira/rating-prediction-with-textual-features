59b407eed705b2ad9577f447ee43df21f3909a2c
emanuellemarreira <emanuellemarreira@gmail.com>
2025-05-22 18:42:22 -0400

    fix: svm parameters

--- code/RQ1/scripts/svm.py
+++ code/RQ1/scripts/svm.py
@@ -3,11 +3,10 @@ from sklearn.svm import SVC as SVM
 
 clf = SVM()
 
-params = { 
-    "C": [0.01, 0.1, 1, 10, 100],
-    "kernel": ["poly", "rbf"],
-    "degree": [3, 4, 5, 6],
-    "gamma": ["scale", "auto", 0.01, 0.1, 1],
+params = {
+  'C': [0.01, 0.1, 1, 10],
+  'kernel': ['linear', 'rbf'],
+  'gamma': ['scale', 'auto', 0.01, 0.1, 1]
 }
 
 model = TrainModel(
